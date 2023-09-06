1e8e9182ff97d29f7dd2b8681a868ffdfe1545b0
Amirul Asraf <amirul.asraf9135@gmail.com>
2023-09-06 11:41:55 +0800

    full configuration

--- lib/Controller/node_config.dart
+++ lib/Controller/node_config.dart
@@ -1,42 +1,68 @@
-
 import 'dart:convert';
 
 import 'package:fukuro_mobile/Controller/fukuro_request.dart';
 import 'package:http/http.dart' as http;
 
-class NodeConfig{
-  static Future<FukuroResponse>  enableNotification(id,metric,val) async {
+class NodeConfig {
+  static Future<FukuroResponse> enableNotification(id, metric, val) async {
     FukuroRequest req = FukuroRequest("config/$id/alert/$metric?val=$val");
-    http.Response htres = await req.post(); 
-    return FukuroResponse(res: htres);
-  }
-  static Future<FukuroResponse>  disableNotification(id,metric) async {
-    FukuroRequest req = FukuroRequest("config/$id/alert/$metric");
-    http.Response htres = await req.del(); 
-    return FukuroResponse(res: htres);
-  }
-  
-  static Future<FukuroResponse>  enableMonitoring(id,metric) async {
-    FukuroRequest req = FukuroRequest("config/$id/monitoring/$metric/toggle");
-    http.Response htres = await req.post(); 
-    return FukuroResponse(res: htres);
-  }
-  static Future<FukuroResponse>  disableMonitoring(id,metric) async {
-    FukuroRequest req = FukuroRequest("config/$id/monitoring/$metric/toggle");
-    http.Response htres = await req.del(); 
-    return FukuroResponse(res: htres);
-  }
-  
-  static Future<FukuroResponse>  loadMetricConfigurations(id,metric) async {
-    FukuroRequest req = FukuroRequest("config/$id/$metric");
-    http.Response htres = await req.get(); 
+    http.Response htres = await req.post();
     return FukuroResponse(res: htres);
   }
 
-  static Future<FukuroResponse>  updateMetricConfigValues(id,metric,payload) async {
+  static Future<FukuroResponse> disableNotification(id, metric) async {
+    FukuroRequest req = FukuroRequest("config/$id/alert/$metric");
+    http.Response htres = await req.del();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> enableMonitoring(id, metric) async {
+    FukuroRequest req = FukuroRequest("config/$id/monitoring/$metric/toggle");
+    http.Response htres = await req.post();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> disableMonitoring(id, metric) async {
+    FukuroRequest req = FukuroRequest("config/$id/monitoring/$metric/toggle");
+    http.Response htres = await req.del();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> loadMetricConfigurations(id, metric) async {
+    FukuroRequest req = FukuroRequest("config/$id/$metric");
+    http.Response htres = await req.get();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> updateMetricConfigValues(
+      id, metric, payload) async {
     FukuroRequest req = FukuroRequest("config/$id/monitoring/$metric");
     req.addBody(payload);
-    http.Response htres = await req.post(); 
+    http.Response htres = await req.post();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> loadPush(id) async {
+    FukuroRequest req = FukuroRequest("config/$id/push");
+    http.Response htres = await req.get();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> updatePush(id, val) async {
+    FukuroRequest req = FukuroRequest("config/$id/push");
+    req.addBody({"interval": val});
+    http.Response htres = await req.post();
+    return FukuroResponse(res: htres);
+  }
+
+  static Future<FukuroResponse> togglePush(id, bool enable) async {
+    FukuroRequest req = FukuroRequest("config/$id/push/toggle");
+    http.Response htres;
+    if (enable) {
+      htres = await req.post();
+    } else {
+      htres = await req.del();
+    }
     return FukuroResponse(res: htres);
   }
 }
--- lib/View/Component/Node/config/node_config_screen.dart
+++ lib/View/Component/Node/config/node_config_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fukuro_mobile/Controller/fukuro_request.dart';
+import 'package:fukuro_mobile/Controller/node_config.dart';
+import 'package:fukuro_mobile/Controller/utilities.dart';
 import 'package:fukuro_mobile/Model/node.dart';
+import 'package:fukuro_mobile/View/Component/Misc/fukuro_dialog.dart';
 import 'package:fukuro_mobile/View/Component/Node/config/cpu_config.dart';
 import 'package:fukuro_mobile/View/Component/Node/config/disk_config.dart';
 import 'package:fukuro_mobile/View/Component/Node/config/mem_config.dart';
@@ -16,12 +20,18 @@ class NodeConfigScreen extends StatefulWidget {
 class NodeConfigScreenState extends State<NodeConfigScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+   final TextEditingController txtPushInterval = TextEditingController();
+  final Map<dynamic,dynamic> mainconfig = {
+    "active":true,
+    "interval":60
+  };
 
   @override
   void initState() {
-    _tabController = TabController(length: 5, vsync: this);
-    _tabController.addListener(_onTabChange);
     super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(_onTabChange);
+    _initialize();
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {});
   }
@@ -31,8 +41,30 @@ class NodeConfigScreenState extends State<NodeConfigScreen>
     return DefaultTabController(
         length: 3,
         child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: Container(child: Row(children: [ 
+              const Text('Monitoring Configuration',style: TextStyle(color: Colors.blue),),
+              Switch(value: mainconfig['active']??true, onChanged: (value) {
+                          _togglePush();
+                        }),
+              Spacer(),
+              Expanded(child: 
+              TextFormField(
+                
+                controller: txtPushInterval,
+                keyboardType: TextInputType.number,  
+                decoration:  InputDecoration(
+                  prefix: const Text('Push Interval(sec):   ',style: TextStyle(color: Colors.blue)),
+                  suffix: IconButton(icon: const Icon(Icons.check,color: Colors.blue,),onPressed:  (){
+                    _savePush();
+                  },), 
+                ),
+              ),), 
+          ]),)
+          ,backgroundColor: Colors.white ,),
           body: CustomScrollView(
-            slivers: [
+            slivers: [ 
               SliverPersistentHeader(
                 delegate: _SliverAppBarDelegate(
                   tabBar: TabBar(
@@ -50,7 +82,7 @@ class NodeConfigScreenState extends State<NodeConfigScreen>
               SliverFillRemaining(
                 child: TabBarView(
                   controller: _tabController,
-                  children: [Text('1'),
+                  children: [ 
                   CPUConfiguration(node: widget.node),
                   MEMConfiguration(node: widget.node),
                   DISKConfiguration(node: widget.node),
@@ -66,10 +98,93 @@ class NodeConfigScreenState extends State<NodeConfigScreen>
     print("change");
     setState(() {});
   }
+
+  _initialize() async {
+    FukuroResponse res = await NodeConfig.loadPush(widget.node.getNodeId());
+    if(res.ok()){
+      print('jere');
+      print(res.body());
+      mainconfig['active'] = res.body()['active']??false;
+      mainconfig['interval'] = res.body()['interval']??60;
+      
+      txtPushInterval.text = (mainconfig['interval']??0).toString();
+      setState(() {
+        
+      });
+
+    }
+    else{
+      if(mounted){
+        
+      FukuroDialog.error(context, 'Failed to load push metric configuration', res.msg());
+      }
+    }
+  }
+  _togglePush()async {
+    try{
+      bool current = mainconfig['active'] == true; 
+      FukuroDialog msg;
+      if(current){ 
+        msg = FukuroDialog(
+          title: "Disable Metric Push",
+          message:
+              "Disabling Metric Pushing will result to no metric will be saved into the system ignoring all other configurations",
+          mode: FukuroDialog.INFO,
+          NoBtn: true, 
+          BtnText: "Yes",
+        );
+      }
+      else{
+        msg = FukuroDialog(
+          title: "Enable Metric Push",
+          message:
+              "Metric will be extracted as per configured and pushed to the server at the determined interval",
+          mode: FukuroDialog.INFO,
+          NoBtn: true, 
+          BtnText: "Yes",
+        );
+      }
+      
+    showDialog(context: context, builder: (_) => msg).then((value) async {
+      if(msg.okpressed){
+        FukuroResponse res = await NodeConfig.togglePush(widget.node.getNodeId(), !current);
+        if(res.ok()){
+          if(mounted){
+            
+            FukuroDialog.success(context, "Saved",'');
+            _initialize();
+          }
+        }
+        else{
+          if(mounted){
+            FukuroDialog.error(context, "Failed", res.msg());
+          }
+        }
+      }
+    });
+    }catch(e){
+      print('error test $e');
+    }
+  }
+  _savePush() async {
+    double val = myPareseNum(txtPushInterval.text);
+    FukuroResponse res = await NodeConfig.updatePush(widget.node.getNodeId(), val);
+    if(mounted){
+      
+    if(res.ok()){
+      FukuroDialog.success(context, 'Saved', '');
+      _initialize();
+    }
+    else{
+      FukuroDialog.error(context, 'Failed to Save', res.msg());
+    }
+    }
+
+  }
+
 }
 
-const _tabs = [
-  Tab(icon: Icon(Icons.settings), text: "Main"),
+const _tabs = [ 
   Tab(icon: Icon(Icons.memory), text: "CPU"),
   Tab(icon: Icon(Icons.dashboard), text: "RAM"),
   Tab(icon: Icon(Icons.layers), text: "Disk"),
@@ -105,3 +220,4 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
     return false;
   }
 }
+ 
